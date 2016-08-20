11e7d053b7f24709fb6165ec4622adf9e11c4bdc
KhainTCore <jxlescallette1@buffs.wtamu.edu>
2016-08-19 23:12:02 -0700

    Set up angular2 root component/module and main.ts. Set up angular2 config files. Set up .gitignore. Set up navBar component.

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+//Decorator
+@Component({
+  selector: "root",
+  templateUrl: "app/app.component.html",
+  styleUrls: ["app/navBar/custom.css"]
+})
+
+//Export to be used elsewhere
+export class AppComponent {}
